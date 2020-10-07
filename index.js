49805e5781f126d3d27cdd5b884eddee2be8676d
John Jeevon Ang <johnjeevonang@gmail.com>
2020-10-07 09:26:34 +0800

    update metric threshold

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const rds = require('./RDS');
 const dynamodb = require('./DynamoDB');
 const lambda = require('./Lambda');
 const ec2 = require('./ec2')
+const threshold = require('./threshold')
 
 rds();
 dynamodb();
 lambda();
 ec2();
+threshold();
---
+++ threshold/index.js
@@ -0,0 +1,42 @@
+const fsExtra = require('fs-extra');
+const fs = require('fs').promises;
+var parse = require('csv-parse/lib/sync');
+
+module.exports = async () => {
+    const fileContent = await fs.readFile(__dirname+'/Threshold.csv');
+    const records = parse(fileContent, {columns: true});
+    const result = {};
+
+    for (let a = 0; a < records.length; a++) {
+        let record = records[a];
+        if(result[record.resource_type.toLowerCase()]) {
+            if(result[record.resource_type.toLowerCase()][record.resource_name]) {
+                if(!result[record.resource_type.toLowerCase()][record.resource_name][record.metric_name]) {
+                    result[record.resource_type.toLowerCase()][record.resource_name][record.metric_name] = {}
+                }
+
+                result[record.resource_type.toLowerCase()][record.resource_name][record.metric_name][record.threshold]  = parseInt(record.metric_value);
+            } else {
+                result[record.resource_type.toLowerCase()][record.resource_name] = {}
+                result[record.resource_type.toLowerCase()][record.resource_name][record.metric_name] = {}
+                result[record.resource_type.toLowerCase()][record.resource_name][record.metric_name][record.threshold]  = parseInt(record.metric_value);
+            }
+        } else {
+            result[record.resource_type.toLowerCase()] = {}
+            result[record.resource_type.toLowerCase()][record.resource_name] = {}
+            result[record.resource_type.toLowerCase()][record.resource_name][record.metric_name] = {}
+            result[record.resource_type.toLowerCase()][record.resource_name][record.metric_name][record.threshold]  = parseInt(record.metric_value);
+        }
+    }
+
+    const path = __dirname + '/../../centralizedlogger/lambda/cloudwatch-metrics';
+    if(!fsExtra.existsSync(path)) {
+        fsExtra.mkdirSync(path, { recursive: true})
+    }
+
+    fsExtra.outputFile(path + '/MetricThreshold.js', 'module.exports = ' + JSON.stringify(result), function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+};
